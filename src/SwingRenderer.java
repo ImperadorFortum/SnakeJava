292617105a048bce369ad872b7ab79accdde9129
joedsonnascimento <joedsonicone@gmail.com>
2025-09-22 11:30:31 -0300

    Lógica bridge
    Implementação do brigde usando a pasta renderer como interface e a swingrenderer como pasta para ser inserido o desenho gráfico futuramente

---
+++ src/SwingRenderer.java
@@ -0,0 +1,6 @@
+public class SwingRenderer implements Renderer {
+    public void draw(Snake snake, Food food) {
+        // A adicionar lógica de desenho usando Swing
+        System.out.println("Preparando...");
+    }
+}
